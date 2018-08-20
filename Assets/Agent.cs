13bc1c23eb3a6d02842089f893ee4288f987a1ce
Jonny B <JBlackburn89@gmail.com>
2018-08-20 22:34:30 +0100

    Init commit

---
+++ Assets/Agent.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Agent : MonoBehaviour {
+	public GameObject target;
+	private float maxVelocity;
+	private float maxSteering;
+	private Vector3 velocity;
+
+	// Use this for initialization
+	void Start () {
+		velocity = Vector3.zero;
+		maxVelocity = 4.0f;
+		maxSteering = 0.1f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Seek();
+	}
+
+	private void Seek() {
+		Vector3 desiredVelocity = Vector3.Normalize(target.transform.position - transform.position) * maxVelocity;
+		desiredVelocity.z = 0.0f;
+
+		Vector3 steering = desiredVelocity - velocity;
+		steering.z = 0.0f;
+		if (steering.sqrMagnitude > maxSteering * maxSteering) {
+			steering.Normalize();
+			steering *= maxSteering;
+		}
+
+		velocity += steering;
+
+		transform.position += (velocity * Time.deltaTime);
+	}
+}
---
+++ Assets/RandomPoint.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomPoint : MonoBehaviour {
+	public float initialChangeTime = 5.5f;
+	private float changeTime;
+
+	// Use this for initialization
+	void Start () {
+		changeTime = initialChangeTime;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		changeTime -= Time.deltaTime;
+
+		if (changeTime <= 0) {
+			changeTime = initialChangeTime;
+
+			Vector3 newPos = Camera.main.ScreenToWorldPoint(
+				new Vector3(
+					Random.Range(0, Screen.width),
+					Random.Range(0, Screen.height),
+					10.0f
+				)
+			);
+
+			gameObject.transform.position = newPos;
+		}
+	}
+}
